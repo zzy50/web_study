edc210cfa58f4d515ad1a1673dbb822541a308e8
zzy50 <jiyeong3141592@gmail.com>
2023-12-13 13:57:30 +0900

    00

---
+++ day8_js_3_array/array_cheatsheet.js
@@ -0,0 +1,22 @@
+// Making an array:
+const colors = ["red", "orange", "yellow"];
+
+// Arrays are indexed like strings:
+colors[0]; // "red"
+""
+// They have a length:
+colors.length; //3
+
+// Important array methods:
+//push(value) - adds value to the END of an array
+//pop() - removes and returns last value in array
+
+//unshift(val) - adds value to START of an array
+//shift() - removes and returns first element in an array
+
+
+let lst = [1, 2, 3, 4, 5]
+lst.push(6) // lst is [1, 2, 3, 4, 5, 6]
+lst.pop() // return 5
+lst.shift() // return 1
+lst.unshift(0) // lst is [0, 1, 2, 3, 4, 5]
